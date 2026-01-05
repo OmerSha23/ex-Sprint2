337a53ebf7c371a335e45845faa09cba6db3cf63
OmerSha23 <oshahar95@gmail.com>
2026-01-05 21:25:29 +0200

    Add more css and create js files with basic funcs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,2 @@
-'use strict'
+// 'use strict'
 
-
-let gElCanvas
-let gCtx
-
-function onInit() {
-    gElCanvas = document.querySelector('canvas')
-    gCtx = document.querySelector('2d')
-}
--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,5 +1,38 @@
 'use strict'
 
-function renderMeme(){
 
+let gElCanvas
+let gCtx
+let gStartPos = null
+
+
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    resizeCanvas()
+    gElCanvas.addEventListener('mousedown', handleMouseClick)
+
+}
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+
+    gElCanvas.width = elContainer.clientWidth
+}
+
+
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+}
+function handleMouseClick(ev) {
+    const { offsetX, offsetY } = ev
+    if (!gStartPos) {
+        gStartPos = { x: offsetX, y: offsetY }
+    } else {
+        drawImg(gStartPos.x, gStartPos.y, offsetX, offsetY)
+        gStartPos = null
     }
+
+}
